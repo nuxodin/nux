d6c3ef5ca7b5c16d2a46448e64974d2bb235826f
Tobias Buschor <tobias.buschor@shwups.ch>
2023-01-31 00:36:38 +0100

    Update js.js

--- util/js.js
+++ util/js.js
@@ -1,38 +1,38 @@
 
 export function mixin(src, target, force, deep) {
     target = target || {};
-    for (let k in src) {
-    	if (!src.hasOwnProperty(k)) continue;
+    for (const k in src) {
+        if (!src.hasOwnProperty(k)) continue;
         if (force || target[k] === undefined) {
             target[k] = src[k];
         }
-		if (!deep) continue;
-		if (typeof target[k] === 'string') continue;
+        if (!deep) continue;
+        if (typeof target[k] === 'string') continue;
         mixin(src[k], target[k], force, deep);
     }
     return target;
-};
+}
 
 
 /**
- * Wraps the Object in a Proxy to allow autovivification
- * @param {object} variable - Object to autoviv
- * @example
- *   var obj = {};
- *   autoviv(obj).x.y.z = {};
- *   console.log(obj)
- * @return {void}
- */
+* Wraps the Object in a Proxy to allow autovivification
+* @param {object} variable - Object to autoviv
+* @example
+*   var obj = {};
+*   autoviv(obj).x.y.z = {};
+*   console.log(obj)
+* @return {void}
+*/
 
-var autoviv = function(variable){
-	return new Proxy(variable, {
-      get: function(target, name) {
-        if (name in target) {
-        	return target[name];
-        } else {
-          target[name] = Object.create(null);
-          return autoviv(target[name])
+const autoviv = function(variable){
+    return new Proxy(variable, {
+        get: function(target, name) {
+            if (name in target) {
+                return target[name];
+            } else {
+                target[name] = Object.create(null);
+                return autoviv(target[name])
+            }
         }
-      }
-   })
+    });
 }
